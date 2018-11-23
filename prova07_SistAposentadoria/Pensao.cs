753193e9f160d3e4992ee8c7407d9f592109f206
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2018-11-22 21:10:51 -0400

    q7p1

---
+++ prova07_SistAposentadoria/Pensao.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace prova07_SistAposentadoria
+{
+    public class Pensao
+    {
+        public string Nome;
+        public int Idade;
+        public string Sexo;
+
+        public void Aposenta()
+        {
+
+            if (Sexo == "feminino")
+            {
+                if (Idade >= 60)
+                {
+                    Console.Write("A Senhora {0} Pode Se Aposentar", Nome);
+                }
+            }
+            else{
+                Console.Write("Essa Pessoa ainda nao Pode Se Aposentar");
+            }
+
+
+                if (Sexo == "masculino")
+            {
+                if (Idade >= 65)
+                {
+                    Console.Write("O Senhor {0} Pode Se Aposentar", Nome);
+                }
+            }
+            else
+            {
+                Console.Write("Essa Pessoa ainda nao Pode Se Aposentar");
+            }
+
+
+        }
+            public Pensao()
+            {
+            }
+
+    }
+}
---
+++ prova07_SistAposentadoria/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace prova07_SistAposentadoria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pensao vence = new Pensao();
+
+            Console.WriteLine("Informe o Nome:");
+            vence.Nome = Console.ReadLine();
+
+            Console.WriteLine("Informe a Idade:");
+            vence.Idade = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o Sexo:");
+            vence.Sexo = Console.ReadLine().ToLower();
+
+            vence.Aposenta();
+        }
+    }
+}
